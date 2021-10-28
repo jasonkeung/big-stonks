2491d3336fea983f0f4bd91b3b6765a3e37321ad
Jason Keung <jkeung@Jasons-MacBook-Pro.local>
2021-10-27 21:10:49 -0700

    Create algorithm file

---
+++ algorithm.py
@@ -0,0 +1,10 @@
+class Algorithm:
+
+    def __init__(self, ticker_history):
+        return None
+
+
+    # Returns: list of buy/sell tuples
+    # Ex: [('B', 2, '43.1'), ('B', 1, 45.21), ('S', 2, 51.54)]
+    def run(self):
+        return None
