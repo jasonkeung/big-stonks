947fff3667ae15944a9150da9d2117a421b5b6a7
Jason Keung <jkeung@Jasons-MacBook-Pro.local>
2021-10-27 21:10:12 -0700

    Create initial files

---
+++ tickerhistory.py
@@ -0,0 +1,10 @@
+import yfinance
+
+class TickerHistory:
+
+    def __init__(self, symbol, interval, range):
+        print('asd')
+    
+    def get_mva(self, range):
+        # TODO: implement
+        print('asd')
