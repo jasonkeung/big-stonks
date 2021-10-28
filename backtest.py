197eba65fb6794c8bb3854c56a38dac044336f38
Jason Keung <jkeung@Jasons-MacBook-Pro.local>
2021-10-27 21:45:52 -0700

    Make first test

--- backtest.py
+++ backtest.py
@@ -11,14 +11,10 @@ class Backtest:
         """
         :return: profit
         """
-        prices = TickerHistory('GOOG', '1d', '30d')
-        alg = NaiveAlgorithm(prices, 10000)
+        history = TickerHistory('GOOG', '1d', '30d')
+        alg = NaiveAlgorithm(history, 10000)
         orders = alg.run()
         profit = Algorithm.get_profit(orders)
         return profit
 
-<<<<<<< HEAD
 print(Backtest.test1())
-=======
-Backtest.test1()
->>>>>>> 4e51b26714fca24f129775a765643c5b6569ab0a
--- naivealg.py
+++ naivealg.py
@@ -5,9 +5,9 @@ class NaiveAlgorithm(Algorithm):
     def run(self):
         """
         :return: list of buy/sell orders
-        Ex: [('B', 2, '43.1'), ('B', 1, 45.21), ('S', 2, 51.54)]
+        Ex: [('B', 2, '43.1'), ('B', 1, 45.21), ('S', 3, 51.54)]
 
         """
         print(self.history.prices)
         
-        return [('B', 2, 43.1), ('B', 1, 45.21), ('S', 2, 51.54)]
+        return [('B', 2, 43.1), ('B', 1, 45.21), ('S', 3, 51.54)]
