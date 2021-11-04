1cbdff8fbcf0825c85dd5736008e1ed2fefcfee6
Jason Keung <jason.keungg@gmail.com>
2021-11-03 19:43:50 -0700

    Change to returning a map

--- naivealg.py
+++ naivealg.py
@@ -5,10 +5,8 @@ class NaiveAlgorithm(Algorithm):
     def run(self):
         """
         :return: list of buy/sell orders
-        Ex: [{'order_type': B', 'num_shares': 2, 'price': 43.1}, {'order_type': B', 'num_shares': 1, 'price': 45.86}, {'order_type': B', 'num_shares': 2, 'price': 51.25}]
+        Ex: [{'order_type': 'B', 'num_shares': 2, 'price': 43.1}, {'order_type': B', 'num_shares': 1, 'price': 45.86}, {'order_type': B', 'num_shares': 2, 'price': 51.25}]
 
         """
         
-        
-
-        return [('B', 2, 43.1), ('B', 1, 45.21), ('S', 3, 51.54)]
+        return [{'order_type': 'B', 'num_shares': 2, 'price': 43.1}, {'order_type': 'B', 'num_shares': 1, 'price': 45.86}, {'order_type': 'B', 'num_shares': 2, 'price': 51.25}]
