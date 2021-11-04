0e2e895f27451748741547c7ea751172289c0e61
Jason Keung <jason.keungg@gmail.com>
2021-11-04 03:03:10 -0700

    Clean up test

--- backtest.py
+++ backtest.py
@@ -1,24 +1,19 @@
+from datetime import datetime
+
 from moontrader import MoonTrader
 from moontrader import MoonTrader
 from ticker import Ticker
 
 class Backtest:
 
-    def testMoonAlg():
-        """
-        :return: profit of MoonTrader
-        """
-        ticker_goog = Ticker('GOOG', '1d', 'ytd')
-        ticker_ge = Ticker('GE', '1d', 'ytd')
-        ticker_amc = Ticker('AMC', '1d', 'ytd')
-        ticker_tsla = Ticker('TSLA', '1d', 'ytd')
-        moontrader = MoonTrader({'GOOG': ticker_goog, 'GE': ticker_ge, 'AMC': ticker_amc, 'TSLA': ticker_tsla}, 10000)
-        
+    def testMoonTrader():
+        ticker_goog = Ticker('TSLA', '1d', 'ytd')
+        ticker_amd = Ticker('TGT', '1d', 'ytd')
+
+        moontrader = MoonTrader([ticker_goog, ticker_amd], 10000)
         moontrader.run()
 
-        print(f'Balance: {moontrader.balance}')
-        print(f'Orders Made: {moontrader.orders}')
-        print(f'Alpha: {moontrader.get_alpha()}')
+        moontrader.print_ending_info()
         
 
-Backtest.testMoonAlg()
+Backtest.testMoonTrader()
