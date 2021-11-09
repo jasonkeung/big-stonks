75ebeda700c91398018a7365ce393c1a8ee4e20f
Jason Keung <jason.keungg@gmail.com>
2021-11-08 23:50:00 -0800

    Create jasontrader and test

--- backtest.py
+++ backtest.py
@@ -7,13 +7,23 @@ from ticker import Ticker
 class Backtest:
 
     def testMoonTrader():
-        ticker_goog = Ticker('TSLA', '1d', 'ytd')
-        ticker_amd = Ticker('TGT', '1d', 'ytd')
+        ticker_goog = Ticker('GE', '1d', 'ytd')
+        ticker_amd = Ticker('AMD', '1d', 'ytd')
 
         moontrader = MoonTrader([ticker_goog, ticker_amd], 10000)
         moontrader.run()
 
         moontrader.print_ending_info()
+    
+    def testJasonTrader():
+        ticker_goog = Ticker('GE', '1d', 'ytd')
+        ticker_amd = Ticker('AMD', '1d', 'ytd')
+
+        jasontrader = JasonTrader([ticker_goog, ticker_amd], 10000)
+        jasontrader.run()
+
+        jasontrader.print_ending_info()
         
 
 Backtest.testMoonTrader()
+Backtest.testJasonTrader()
---
+++ jasontrader.py
@@ -0,0 +1,12 @@
+from trader import Trader
+from order import Order
+
+class JasonTrader(Trader):
+
+    def run(self):
+        """
+        Runs asdasdasdasd
+
+        :return: None
+        """
+        return None
