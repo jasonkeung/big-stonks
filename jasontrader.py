da50afdc47aba1c88b8e03c7be6e076934fd9519
Jason Keung <jason.keungg@gmail.com>
2021-11-10 14:33:55 -0800

    Add trader class comment to subclasses

--- jasontrader.py
+++ jasontrader.py
@@ -2,11 +2,22 @@ from trader import Trader
 from order import Order
 
 class JasonTrader(Trader):
+    """
+    Trader with balance, portfolio, tickers to trade, and orders made.
+
+    Attributes:
+        tickers: Map of symbol: Tickers for the trader to use (all Tickers need to have same period/interval)
+        starting_bal: Starting dollar balance for the trader to use
+        balance: Current balance of the trader
+        portfolio: Map of tickers and quantity held Ex: {'GOOG': 2, 'TSLA': 3}
+        orders: List of all Orders made Ex: [Order('GOOG', 'B', 2, 53.21, datetime(2021, 08, 02)), Order('GOOG', 'S', 2, 64.53, datetime(2021, 10, 21))]
+    """
 
     def run(self):
         """
-        Runs asdasdasdasd
+        Runs a scalping algorithm
 
         :return: None
         """
+        print(self.tickers[0].prices.index)
         return None
