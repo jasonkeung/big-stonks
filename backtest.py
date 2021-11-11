78a2b3462058877761d84bfeeaf1173e325971e9
Henry Cheong <heneong@Henrys-MacBook-Pro.local>
2021-11-10 20:47:05 -0800

    henry changed

--- backtest.py
+++ backtest.py
@@ -23,6 +23,14 @@ class Backtest(unittest.TestCase):
         claudeatrader.run()
 
         claudeatrader.print_ending_info()
+
+    def testHenTayTrader(self):
+        ticker_goog = Ticker('GOOG', '1d', '1y')
+        ticker_tsla = Ticker('TSLA', '1d', '1y')
+
+        hentaytrader = HenTayTrader([ticker_goog], 10000)
+
+        hentaytrader.print_ending_info()
         
 if __name__ == '__main__':
     unittest.main()
