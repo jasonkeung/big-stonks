dc86461d68e73b60dd3d252a5688b09054d44531
Jason Keung <jason.keungg@gmail.com>
2021-11-10 20:51:49 -0800

    Add unittest.skip

--- backtest.py
+++ backtest.py
@@ -1,22 +1,23 @@
 import unittest
 
-from moontrader import MoonTrader
-from claudeatrader import ClaudeaTrader
-from jasontrader import JasonTrader
 from ticker import Ticker
 
 class Backtest(unittest.TestCase):
     
+    # @unittest.skip # uncomment this to skip
     def testJasonTrader(self):
-        ticker_ge = Ticker('GE', '1d', 'ytd')
-        ticker_amd = Ticker('AMD', '1d', 'ytd')
+        from jasontrader import JasonTrader
+        ticker_ge = Ticker('GE', '15m', '1w')
+        ticker_amd = Ticker('AMD', '15m', '1w')
 
-        jasontrader = JasonTrader([ticker_ge, ticker_amd], 10000)
+        jasontrader = JasonTrader([ticker_ge], 10000)
         jasontrader.run()
 
         jasontrader.print_ending_info()
 
+    # @unittest.skip # uncomment this to skip
     def testClaudeaTrader(self):
+        from claudeatrader import ClaudeaTrader
         ticker_msft = Ticker('MSFT', '1d', '1y')
 
         claudeatrader = ClaudeaTrader([ticker_msft], 10000)
