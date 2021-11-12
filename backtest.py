86c0e38ecf86c43a04232d1dbe70094d56c152aa
Jason Keung <jason.keungg@gmail.com>
2021-11-11 16:30:04 -0800

    Use new Ticker.get_tickers function

--- backtest.py
+++ backtest.py
@@ -9,15 +9,14 @@ class Backtest(unittest.TestCase):
     # @unittest.skip # uncomment this to skip
     def testJasonTrader(self):
         from jasontrader import JasonTrader
-        ticker_ge = Ticker('GE', '15m', '1w')
-        ticker_amd = Ticker('AMD', '15m', '1w')
+        tickers = Ticker.get_tickers(['GE', 'AMD', 'F', 'MSFT'], interval='15m', period='1mo')
 
-        jasontrader = JasonTrader([ticker_ge], 10000)
+        jasontrader = JasonTrader(tickers, 10000, eps=0.015, stop_loss=.5)
         jasontrader.run()
-
+        
         jasontrader.print_ending_info()
 
-    # @unittest.skip # uncomment this to skip
+    @unittest.skip # uncomment this to skip
     def testClaudeaTrader(self):
         from claudeatrader import ClaudeaTrader
         ticker_msft = Ticker('MSFT', '1d', '1y')
