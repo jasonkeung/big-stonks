df719073cc83648a3836dfdc2808aeb275c6db0a
Jason Keung <jason.keungg@gmail.com>
2021-11-11 16:05:36 -0800

    Refactor, remove buy logic

--- jasontrader.py
+++ jasontrader.py
@@ -1,5 +1,4 @@
 import math
-from warnings import _catch_warnings_with_records
 
 from trader import Trader
 
@@ -15,17 +14,17 @@ class JasonTrader(Trader):
         orders: List of all Orders made Ex: [Order('GOOG', 'B', 2, 53.21, datetime(2021, 08, 02)), Order('GOOG', 'S', 2, 64.53, datetime(2021, 10, 21))]
     """
 
-    def __init__(self, eps=0.001, stop_loss=.3):
+    def __init__(self, tickers_list, starting_bal, eps=0.001, stop_loss=.3):
         """
         Call's Trader's init and sets custom parameters.
 
         :param eps: (Optional) 
         :param starting_bal: dollar amount for the Trader to trade with
         """
-        super.__init__()
+        super().__init__(tickers_list, starting_bal)
         self.eps = eps
         self.bal_saved = self.balance * (1 - stop_loss)
-        self.balance -= self.bal_saved
+        self.balance -= self.bal_saved # set aside money so we only use self.balance * stop_loss to trade
 
         # map from sym to (map from buyprice to quantity)
         self.init_buy_points = {sym: {} for sym in self.tickers.keys()}
@@ -39,8 +38,6 @@ class JasonTrader(Trader):
         :return: None
         """
         date_index = self.tickers[list(self.tickers.keys())[0]].prices.index # gets the prices index from a random ticker in self.tickers
-        bal_saved = self.balance * .7
-        self.balance -= bal_saved # set aside 80% of starting_bal as a stop loss
 
         for date in date_index:
             for sym, ticker in self.tickers.items():
@@ -49,8 +46,8 @@ class JasonTrader(Trader):
         self.balance += self.bal_saved # add back bal_saved that was set aside
     
     def trade(self, sym, ticker, date):
-        init_ticker_buys = self.init_buy_points[sym]
-        curr_ticker_buys = self.curr_buy_points[sym]
+        init_buys = self.init_buy_points[sym]
+        curr_buys = self.curr_buy_points[sym]
 
         curr_price = ticker.prices["Close"][date]
         
@@ -59,25 +56,27 @@ class JasonTrader(Trader):
             to_spend = self.balance / len(self.tickers) / 2
             num_to_buy = to_spend / curr_price
             self.buy(sym, num_to_buy, curr_price, date)
-            init_ticker_buys[curr_price] = num_to_buy
-            curr_ticker_buys[curr_price] = num_to_buy
+            init_buys[curr_price] = num_to_buy
+            curr_buys[curr_price] = num_to_buy
         else:
             num_to_buy = 0
             num_to_sell = 0
 
-            for buy_price, curr_quantity in curr_ticker_buys.items():
-                init_quantity = init_ticker_buys[buy_price]
+            for buy_price, curr_quantity in curr_buys.items():
+                init_quantity = init_buys[buy_price]
                 price_change = (curr_price - buy_price) / buy_price
                 if price_change > self.eps:
                     # sell a fraction of initial buy quantity, min 1/32 or the rest of it
                     fraction_to_sell = init_quantity / (2 ** (math.floor(price_change / self.eps)))
                     num_to_sell += min(curr_quantity, max(fraction_to_sell, init_quantity / 32))
                     self.curr_buy_points[sym][buy_price] -= num_to_sell
-                elif price_change < -self.eps:
-                    # buy using .1 of balance
-                    num_to_buy += .1 * self.balance / curr_price
-                    # print holdings to see if more than 10
             
+            
+            # buy if 30 mva > 180 mva
+            
+
+
+            # compare buys and sells to make the net order
             num_change = num_to_buy - num_to_sell
             if num_change > 0:
                 self.buy(sym, num_change, curr_price, date)
